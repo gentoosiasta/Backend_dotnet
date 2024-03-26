9197538900b32d19b707cf73cd2a2e7d78ae10d1
Julio Aguilar Carmona <julio.aguilarc@gmail.com>
2024-03-26 01:10:21 -0600

    Fix Acceso model

--- Models/Acceso.cs
+++ Models/Acceso.cs
@@ -7,22 +7,26 @@ namespace Backend_dotnet.Models
     {
         [Key]
         [StringLength(30)]
-        public string Usuario { get; set; }
+        public string? Usuario { get; set; }
 
         [StringLength(100)]
-        public string Nombre { get; set; }
+        public string? Nombre { get; set; }
 
         [Required]
         [StringLength(100)]
-        public string Password { get; set; }
+        public string? Password { get; set; }
 
         [Required]
         [StringLength(50)]
-        public string Email { get; set; }
+        public string? Email { get; set; }
 
         [Required]
         [StringLength(3)]
-        public string Tipo_usuario { get; set; }
+        public string? Tipo_usuario { get; set; }
+
+        [Required]
+        [StringLength(1)]
+        public string? Status { get; set; }
 
         [DataType(DataType.DateTime)]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
