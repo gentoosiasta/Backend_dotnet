07d8b79e93ece9524a833f9e845b7f96004b0743
Julio Aguilar Carmona <julio.aguilarc@gmail.com>
2024-04-13 00:30:40 -0600

    :sparkles: Add CourseSupportMaterial model

---
+++ Models/CourseSupportMaterial.cs
@@ -0,0 +1,34 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Backend_dotnet.Models
+{
+    public class CourseSupportMaterial
+    {
+        [Key, Column(Order = 0)]
+        [ForeignKey("Period")]
+        public virtual SchoolPeriod? Period { get; set; }
+
+        [Key, Column(Order = 1)]
+        [ForeignKey("GroupID")]
+        public virtual Group? Group { get; set; }
+
+        [Key, Column(Order = 2)]
+        [ForeignKey("SubjectID")]
+        public virtual Subject? Subject { get; set; }
+
+        [Key, Column(Order = 3)]
+        [ForeignKey("RFC")]
+        public virtual Staff? Staff { get; set; }
+
+        [Key, Column(Order = 4)]
+        public int Topic { get; set; }
+
+        [Key, Column(Order = 5)]
+        public int Consecutive { get; set; }
+
+        [Required]
+        [StringLength(250)]
+        public string? Description { get; set; }
+    }
+}
--- obj/Debug/net8.0/Backend_dotnet.AssemblyInfo.cs
+++ obj/Debug/net8.0/Backend_dotnet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend_dotnet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09c6c8dabb0aaa24fdcd352a67b3d0a7cd2d2dc1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca69e80f160b97ac25252da05b2a882130fa19b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend_dotnet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend_dotnet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
