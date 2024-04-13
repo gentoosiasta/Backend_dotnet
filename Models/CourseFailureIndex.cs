09c6c8dabb0aaa24fdcd352a67b3d0a7cd2d2dc1
Julio Aguilar Carmona <julio.aguilarc@gmail.com>
2024-04-13 00:21:51 -0600

    :sparkles: Add CourseFailureIndex model

---
+++ Models/CourseFailureIndex.cs
@@ -0,0 +1,30 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Backend_dotnet.Models
+{
+    public class CourseFailureIndex
+    {
+        [Key, Column(Order = 0)]
+        [ForeignKey("Period")]
+        public virtual SchoolPeriod? Period { get; set; }
+
+        [Key, Column(Order = 1)]
+        [ForeignKey("GroupID")]
+        public virtual Group? Group { get; set; }
+
+        [Key, Column(Order = 2)]
+        [ForeignKey("SubjectID")]
+        public virtual Subject? Subject { get; set; }
+
+        [Key, Column(Order = 3)]
+        [ForeignKey("RFC")]
+        public virtual Staff? Staff { get; set; }
+
+        [Key, Column(Order = 4)]
+        public int Unit { get; set; }
+
+        [Column(TypeName = "decimal(10,2)")]
+        public decimal FailureIndex { get; set; }
+    }
+}
